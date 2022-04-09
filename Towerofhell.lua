2ec0bd2ca7054fe5c761490485285e833fffc44c
Stang001 <90966008+Stang001@users.noreply.github.com>
2022-04-09 19:32:50 +0700

    Create Towerofhell.lua

---
+++ Towerofhell.lua
@@ -0,0 +1,71 @@
+local placeId=game.PlaceId
+	if placeId == 1962086868 then
+		noob=true
+	elseif placeId == 3582763398 then
+		pro=true
+    else
+            game.Players.LocalPlayer:Kick('Only Tower of hell')
+    end
+local l=loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt")()
+local w=l:Window("DogeX Open Source")
+local jdfajjsjalfjsldfl=w:Server("Only Practice!", "")
+local m=jdfajjsjalfjsldfl:Channel("AGHHHHHHH","")
+local get = setmetatable({}, {
+    __index = function(self, key)
+        return game:GetService(key)
+    end
+})
+function WOWyouisGAYS(title,text,time)
+    game.StarterGui:SetCore("SendNotification", {
+        Title = title;
+        Text = text;
+        Duration = time;
+    })
+end
+local dfdsfsfsf={
+    OMOGGGGMOGMOMGOGOMOGPMOGMGOMGOMOGMGMO=false
+}
+local ri=get.ReplicatedStorage
+local r=get.RunService
+local p=get.Players
+local LocalPlayer=p.LocalPlayer
+local lp=p.LocalPlayer
+local root=lp.Character.HumanoidRootPart
+m:Button('Bypass 1% LOL',function()
+    local dasiojfosdjp=ri:FindFirstChild("getBans")
+        if dasiojfosdjp then
+            dasiojfosdjp:Destroy()
+        end
+end)
+m:Toggle('God Mode',false,function(a)
+    dfdsfsfsf.OMOGGGGMOGMOMGOGOMOGPMOGMGOMGOMOGMGMO=a
+end)
+    m:Button('win',function() root.CFrame=get.Workspace.tower.finishes.Finish.CFrame wait(.1)   for i, v in pairs(LocalPlayer.Character:GetChildren()) do if v:IsA("BasePart") or v:IsA("Part") or v:IsA("MeshPart") then if v.Name ~= "HumanoidRootPart" then v:Destroy() end end end end)
+    m:Button("All Tools",function(d)
+    for g,e in pairs(ri.Gear:GetChildren()) do
+        e:Clone()
+        wait()
+        e.Parent=lp.Backpack
+    end
+end)
+m:Button('rejoin',function() get.TeleportService:Teleport(game.PlaceId,lp) end)
+m:Button('reset',function() for i, v in pairs(LocalPlayer.Character:GetChildren()) do if v:IsA("BasePart") or v:IsA("Part") or v:IsA("MeshPart") then if v.Name ~= "HumanoidRootPart" then v:Destroy()
+                end
+            end
+        end
+    end)
+r.Heartbeat:Connect(function()
+    if dfdsfsfsf.OMOGGGGMOGMOMGOGOMOGPMOGMGOMGOMOGMGMO then
+for Myball,Dick in pairs(lp.Character:GetChildren()) do
+    if Dick.Name == "hitbox" then
+        Dick.Parent=ri
+    end
+end
+else
+    for asshole,SIMP in pairs(ri:GetChildren()) do
+        if SIMP.Name == "hitbox" then
+            SIMP.Parent=lp.Character
+        end
+    end
+end
+end)
